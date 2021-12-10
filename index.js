f63bb8df073bb21a313da3973c4f658293deeff1
iNeatz <sahilshrestha77@gmail.com>
2021-12-10 22:45:23 +0545

    new changes to package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 const requests = require('requests')
+const port = process.env.PORT || 8000
 
 const homeFile = fs.readFileSync("home.html", "utf-8")
 
@@ -32,4 +33,4 @@ const server = http.createServer((req, res) => {
     }
 })
 
-server.listen(8000, "127.0.0.1")
+server.listen(port, "127.0.0.1")
